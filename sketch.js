736445248d0fc17652957d9758813aadbb966f8a
Karan2124 <104508615+Karan2124@users.noreply.github.com>
2022-05-19 16:40:41 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var family = { 
+father : "Chandrahas",
+mother : "Akshatha",
+me : "Karan",
+sister : "Khushi"
+
+}
+
+
+
+
+
+
+function setup() {
+  createCanvas(400, 400);
+  console.log(family.me)
+}
+
+function draw() {
+  background(220);
+}
